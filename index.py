5fc4a678066d5080be6dea95e25d2307d990db6a
Paul <72017436+paul-baiju@users.noreply.github.com>
2021-07-06 17:39:34 +0530

    paul

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
-import os
-
-# detect the current working directory and print it
-path = os.getcwd()
-print ("The current working directory is %s" % path)
+import requests
+r = requests.get('https://github.com/kennethreitz/requests/blob/master/README.rst')
+'Requests:' in r.text
+r.headers['Content-Type']
+'text/html; charset=utf-8'
+r = requests.get('https://raw.github.com/kennethreitz/requests/master/README.rst')
+'Requests:' in r.text
+r.headers['Content-Type']
+'text/plain; charset=utf-8'
+print (r.text)
