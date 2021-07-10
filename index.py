6738a57205ee69179cb94f1a2dc157c8a316c013
Paul <72017436+paul-baiju@users.noreply.github.com>
2021-07-10 19:58:04 +0530

    paul

--- index.py
+++ index.py
@@ -1,10 +1,27 @@
 import requests
-r = requests.get('https://github.com/kennethreitz/requests/blob/master/README.rst')
-'Requests:' in r.text
-r.headers['Content-Type']
-'text/html; charset=utf-8'
-r = requests.get('https://raw.github.com/kennethreitz/requests/master/README.rst')
+
+print("Select file type:")
+options = ["html", "php"]
+
+def let_user_pick(options):
+    for idx, element in enumerate(options):
+        print("{}) {}".format(idx+1,element))
+    i = input("Enter number: ")
+    try:
+        if 0 < int(i) <= len(options):
+            return (options[int(i)-1])
+    except:
+        pass
+    return None
+
+selected_option = let_user_pick(options)
+print(selected_option)
+
+r = requests.get('https://raw.githubusercontent.com/paul-baiju/Automation-boilerplate/main/resources/index.html')
 'Requests:' in r.text
 r.headers['Content-Type']
 'text/plain; charset=utf-8'
-print (r.text)
+
+f = open('index.html','w')
+f.write(r.text)
+f.close()
