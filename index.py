bf9a1dc69434866c4befb1614aaaf8e73e4df696
Paul <72017436+paul-baiju@users.noreply.github.com>
2021-07-11 16:46:12 +0530

    paul

--- index.py
+++ index.py
@@ -1,10 +1,22 @@
 import requests
+import os
+from bs4 import BeautifulSoup as Soup
 
 def resource_finder(selected_option):
     return {
-        'html': 'https://raw.githubusercontent.com/paul-baiju/Automation-boilerplate/main/resources/index.html',
-        'php': 2,
+        'html': 'https://raw.githubusercontent.com/paul-baiju/Automation-boilerplate/main/resources/base/index.html',
+        'php': 'https://raw.githubusercontent.com/paul-baiju/Automation-boilerplate/main/resources/base/index.html',
+        'css': 'https://raw.githubusercontent.com/paul-baiju/Automation-boilerplate/main/resources/base/index.css'
     }[selected_option]
+
+def add_css(data_css):
+    if not os.path.exists("css/"):
+        os.makedirs("css/")
+        f = open('css/style.css','w')
+    else:
+        f = open('css/style.css','a')
+    f.write(data_css)
+    f.close()
 
 print("Select file type:")
 options = ["html", "php"]
@@ -12,7 +24,7 @@ options = ["html", "php"]
 def let_user_pick(options):
     for idx, element in enumerate(options):
         print("{}) {}".format(idx+1,element))
-    i = input("Enter number: ")
+    i = input("Enter option number: ")
     try:
         if 0 < int(i) <= len(options):
             return (options[int(i)-1])
@@ -21,12 +33,28 @@ def let_user_pick(options):
     return None
 
 selected_option = let_user_pick(options)
+print(selected_option)
 
 r = requests.get(resource_finder(selected_option))
 'Requests:' in r.text
 r.headers['Content-Type']
 'text/plain; charset=utf-8'
 
-f = open('index.html','w')
-f.write(r.text)
+pageData = r.text
+soupData = Soup(pageData, "html.parser")
+title = soupData.find('title')
+
+data_title = input("Enter the title: ")
+title.insert(1,data_title)    
+
+if selected_option == "php":
+    f = open('index.php','w')
+else:
+    f = open('index.html','w')
+
+r = requests.get(resource_finder("css"))
+add_css(r.text)
+
+pageData = str(soupData)
+f.write(pageData)
 f.close()
